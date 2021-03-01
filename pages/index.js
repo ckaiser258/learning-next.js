80411a8bd7ffdadd690c2ffc318b55ed13f4645d
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-01 11:41:43 -0700

    Add NavBar and Footer to index.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
@@ -19,6 +22,7 @@ export default function Home() {
         Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
         tempus augue sed malesuada ultricies.
       </p>
+      <Footer />
     </div>
   );
 }
