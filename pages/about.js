d6d0cd204938fe390e1d2e62ffd926e17a876565
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-01 11:34:47 -0700

    Create about page.

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
+        sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
+        fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
+        Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
+        tempus augue sed malesuada ultricies.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
+        sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
+        fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
+        Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
+        tempus augue sed malesuada ultricies.
+      </p>
+    </div>
+  );
+};
+
+export default About;
