451328aaa68ad9d8f9e06f22c827627e09e135b5
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-03 14:08:59 -0700

    Implement homepage styles.

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,15 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
         sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
         fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
         Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
         tempus augue sed malesuada ultricies.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
         sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
         fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
@@ -23,7 +23,7 @@ export default function Home() {
         tempus augue sed malesuada ultricies.
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
