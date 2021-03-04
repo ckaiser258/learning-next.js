c66619e392b298b7c92b120823a9bdfa4d9308a9
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-04 12:11:48 -0700

    Create 404.js.

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+//If a url doesn't exist, it will automatically serve up this page
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>Ooooops...</h1>
+      <h2>That page cannot be found.</h2>
+      <p>
+        Go back to the{" "}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
