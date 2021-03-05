53f886f2df87ed903ce2108b27305354515839ad
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-05 11:41:26 -0700

    Add Head component and metadata to about page.

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,30 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
-        sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
-        fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
-        Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
-        tempus augue sed malesuada ultricies.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
-        sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
-        fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
-        Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor. Nam
-        tempus augue sed malesuada ultricies.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
+          sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
+          fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
+          Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor.
+          Nam tempus augue sed malesuada ultricies.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ex
+          sapien. Vestibulum sit amet viverra arcu. Aenean sed turpis vel erat
+          fringilla gravida ut id lectus. Aliquam finibus non ex id euismod.
+          Aliquam gravida leo nec auctor lacinia. Integer ut malesuada tortor.
+          Nam tempus augue sed malesuada ultricies.
+        </p>
+      </div>
+    </>
   );
 };
 
