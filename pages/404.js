c5a8e5b78bab663984a7245dec3d0bcf2c5c378d
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-05 11:27:33 -0700

    Add ability to redirect to homepage after 404.

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 //If a url doesn't exist, it will automatically serve up this page
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(1)
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
