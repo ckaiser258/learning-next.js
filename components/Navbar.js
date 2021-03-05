0ade009e742bb3bd1a5bba4a3acb529a0ef09600
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-05 11:41:48 -0700

    Add logo to navbar using Image tag from next.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        {/* Image from nextjs forces us to use width and height properties, otherwise throws an error 
+          and also makes the image responsive based on those properties.
+          It also optimizes loading speeds by only loading the image once it's in view
+        */}
+        <Image src="/logo.png" width={128} height={77} />
       </div>
       {/* href gets added to the Link tag and the a tag stays empty */}
       <Link href="/">
