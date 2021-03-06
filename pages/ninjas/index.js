32019ad69cce2437fe5dc33dc58b9e175b107a19
Colton <kaiserc2@seattleu.edu>
2021-03-05 19:42:56 -0700

    Fetch and render ninjas. Delete test.js.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,33 @@
-const Ninjas = () => {
-  return <h1>All Ninjas</h1>;
+import styles from '../../styles/Ninjas.module.css'
+
+//getStaticProps runs at build time, NOT in the browser
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    //need to create this props object to use the data within the component
+    props: { ninjas: data },
+  };
+};
+
+//props.ninjas is coming from above
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(
+        (ninja => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Ninjas;
