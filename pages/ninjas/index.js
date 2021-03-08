a8ef26e9f1565ef7ed7ab982ee80786898536eac
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-08 09:29:36 -0700

    Replace div rendering each ninja with a Link tag.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
-import styles from '../../styles/Ninjas.module.css'
+import Link from "next/link";
+import styles from "../../styles/Ninjas.module.css";
 
 //getStaticProps runs at build time, NOT in the browser
 
@@ -17,15 +18,13 @@ const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
-      {ninjas.map(
-        (ninja => (
-          <div key={ninja.id}>
-            <a className={styles.single}>
-              <h3>{ninja.name}</h3>
-            </a>
-          </div>
-        ))
-      )}
+      {ninjas.map((ninja) => (
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
